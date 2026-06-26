be04d842b43632ecb08881053977e62ae8bbe27b
YoungOnion <39562198+YoungOnionMC@users.noreply.github.com>
2026-06-26 00:07:36 -0600

    fix menu panels not transforming with non zeroed viewports

--- src/main/java/brachy/modularui/widgets/menu/MenuPanel.java
+++ src/main/java/brachy/modularui/widgets/menu/MenuPanel.java
@@ -1,10 +1,13 @@
 package brachy.modularui.widgets.menu;
 
+import brachy.modularui.api.layout.IViewportStack;
 import brachy.modularui.api.widget.IWidget;
 import brachy.modularui.screen.ModularPanel;
+import brachy.modularui.screen.viewport.ModularGuiContext;
 import brachy.modularui.widget.WidgetTree;
 
 import org.jetbrains.annotations.ApiStatus;
+import org.joml.Matrix4f;
 
 import java.util.List;
 
@@ -33,6 +36,16 @@ public class MenuPanel extends ModularPanel<MenuPanel> {
     protected void onChildAdd(IWidget child) {
         super.onChildAdd(child);
         child.scheduleResize();
+    }
+
+    @Override
+    public void transform(IViewportStack stack) {
+        ModularGuiContext context = getContext();
+        if (context != null) {
+            Matrix4f pose = context.getLastGraphicsPose();
+            stack.translate(pose.m30(), pose.m31());
+        }
+        super.transform(stack);
     }
 
     @Override
